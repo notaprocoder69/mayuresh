47ddb107008560d36c68706a78f73384c4c68f17
Mayuresh <151500158+notaprocoder69@users.noreply.github.com>
2024-08-15 14:19:56 +0530

    Update electricitybillcalculator.c

--- electricitybillcalculator.c
+++ electricitybillcalculator.c
@@ -1,6 +1,6 @@
-/* in the project i am using the electricity usage of an individual to calculate and store the amount 
-he has to pay every month. the project also gives warning to the user if he/she exceeds the monthly usage 
-of power limit */
+/* In the project i am using the electricity usage of an individual to calculate and store the amount 
+he/she has to pay every month. the project also gives warning to the user if he/she exceeds the monthly usage of power limit. lets the user decide to store, delete or display the details. it also gives recommendation based on average unit consumption
+ */
 
 
 
